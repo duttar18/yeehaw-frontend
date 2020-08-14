50f1d613ee0942d9d20e7c5279eb0901145f66ea
dbzheng <dbzheng@umich.edu>
2020-08-14 16:37:38 -0400

    added menu and nav

---
+++ src/Nav.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export class Nav extends Component {
+    render() {
+        return (
+            <nav>
+                <h3>Logo</h3>
+                <ul>
+                    <li>About</li>
+                    <li>Rules</li>
+                </ul>
+            </nav>
+        )
+    }
+}
+
+export default Nav
--- src/Welcome.js
+++ src/Welcome.js
@@ -1,21 +1,21 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 import img from './Assets/Background.png';
-
+import Menu from './Menu';
 
 
 const Welc = styled.div`
     width: 100%;
     display: flex; 
-    justify-content: space-around; 
     height: 100vh;
     background-image: url(${img});
     background-size: cover
 `;
 const Shooter = styled.img`
     height: 400px;
-    margin-top: 750px;
-    margin-left: -900px;
+    position: fixed;
+    margin-top: 700px;
+    margin-left: 400px;
 
 `;
 export class Welcome extends Component {
@@ -23,6 +23,8 @@ export class Welcome extends Component {
         return (
             <Welc>
                 <Shooter src={require("./Assets/Shooter.gif")} />
+                <Menu />
+                
             </Welc>
         );
     }
