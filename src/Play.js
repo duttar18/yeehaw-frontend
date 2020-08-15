4d4495d84dc2697296007d8ec3c70b8ade47cacf
Raul Dutta <duttar@umich.edu>
2020-08-14 20:38:46 -0400

    music

--- src/Play.js
+++ src/Play.js
@@ -1,11 +1,13 @@
-
+import "./Play.css"
 import React from 'react';
 import {Link} from "react-router-dom";
 import { withRouter, Router, NavLink, Route, Switch, useLocation } from 'react-router-dom';
 
 function Play() {
   return (
-      <div>professional resumesss</div>
+      <div>professional resumesss
+
+      </div>
   );
 }
 
